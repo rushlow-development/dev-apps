beb74eb7c18783b800e83b7ed0f3440f04d2ef85
Jesse Rushlow <jr@rushlow.dev>
2024-03-01 04:01:45 -0500

    handle symlinking files

--- src/Command/LinkCommand.php
+++ src/Command/LinkCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 
@@ -34,11 +35,25 @@ class LinkCommand extends Command
             return Command::FAILURE;
         }
 
-        if ($fs->exists($targetPath)) {
+        if (!is_dir($sourcePath)) {
+            // The source is a file, create the link.
+            return $this->createLink($sourcePath, $targetPath);
+        }
+
+        $helper = $this->getHelper('question');
+        $question = new ConfirmationQuestion('The target path already exists. Do you want to replace it with the symlink?', false);
+
+        if ($fs->exists($targetPath) && $helper->ask($input, $output, $question)) {
+            // The source is not a dir, the target already exists, & the user wants to remove it.
             $fs->remove($targetPath);
         }
 
-        $process = Process::fromShellCommandline(sprintf('ln -sF %s %s', $sourcePath, $targetPath));
+        return $this->createLink($sourcePath, $targetPath);
+    }
+
+    private function createLink(string $source, string $target): int
+    {
+        $process = Process::fromShellCommandline(sprintf('ln -s %s %s', $source, $target));
         return $process->run();
     }
 }
